c3cd81b03b9e6e642d0f78672b23db4dfb493c36
Jerry M <developer.jerrymannel@gmail.com>
2023-11-02 15:50:36 +0530

    missed to export s3 file handler

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const db = require("./utils.db");
 const mongodb = require("./utils.mongo");
 const mongooseUtils = require("./utils.mongoose");
+const s3FileHandler = require("./utils.aws.s3");
 
 module.exports = {
 	db,
 	mongodb,
-	mongooseUtils
+	mongooseUtils,
+	s3FileHandler
 }
